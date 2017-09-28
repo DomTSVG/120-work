f09448d6e7c12da113b9c8d45650e7e606a174a9
DomTSVG <iokholeg@gmail.com>
2017-09-28 05:40:13 -0600

    Homework for week 3, part 1

---
+++ hw-3/sketch.js
@@ -0,0 +1,9 @@
+// Fucntion setup
+function setup(){
+  createCanvas( 600, 400 ); // created canvas
+  background( "pink" ); // background
+}
+// Draw function
+function draw(){
+  text( "Hello World!", 20, 30 );
+}
